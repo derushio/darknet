69b04c5fc08bc869a4c73888e3a00e462b185ff4
derushio <derushio@gmail.com>
2020-01-08 17:42:11 +0900

    <master>[modified] change path

--- python/darknet.py
+++ python/darknet.py
@@ -43,9 +43,9 @@ class METADATA(Structure):
                 ("names", POINTER(c_char_p))]
 
 
-
-#lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+from config import ROOT_DIR
+lib = CDLL(ROOT_DIR + "/libs/darknet/libdarknet.so", RTLD_GLOBAL)
+# lib = CDLL("libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
